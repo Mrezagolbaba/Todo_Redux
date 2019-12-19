2962a58a0a54b970f8f70d2681017279b25a9360
Mohamadreza <m.reza.golbaba@gmail.com>
2019-12-19 17:11:30 +0330

    change in edit  note

--- src/components/EditNote.js
+++ src/components/EditNote.js
@@ -4,6 +4,8 @@ import {connect} from "react-redux";
 import {deleteNote, editNote, updateNote} from "../util/Redux/actions";
 import del from'../assets/delete.svg'
 import edit from'../assets/pencil.svg'
+
+
 class EditNote extends Component{
     constructor(props){
         super(props)
@@ -12,33 +14,35 @@ class EditNote extends Component{
         }
 
     }
-    updateInput(e,id) {
-        this.setState({
-            noteText: e.target.value,
-            id:id
-        })
-    }
-    UpdateItem(id){
-        this.props.updateNote(this.state.noteText,this.state.id)
-    }
+    handleEdit = (e) => {
+        e.preventDefault();
+        const newNote = this.getTitle.value;
+        const id= this.props.note.id
+        const data = {
+            newNote,
+        };
+        this.props.updateNote(data,id)
+        console.log(data)
+    };
+
 
     render() {
-        console.log('note',this.props.note.id)
+        console.log('note',this.props.note)
         return (
             <div className="container__row">
-              <div className="container__CardList" key={this.props.note.id} >
-                  <div>
-                                    <input className={'container__CardList__textEdit'} defaultValue={this.props.note.text} onChange={(e)=>this.updateInput(e,this.props.note.id)}/>
-                                </div>
-                  <div>
-                                    <button className="container__CardList__Button" onClick={() => this.props.deleteNote(this.props.note.id)} >
-                                        <img alt='icon' className='container__CardList__Button_icon' src={del}/>
-                                    </button>
-                                    <button className="container__CardList__Button" onClick={() => this.UpdateItem(this.props.note.id) }>
-                                        <img alt='icon' className='container__CardList__Button_icon' src={edit}/>
-                                    </button>
-                                </div>
-              </div>
+                <form onSubmit={this.handleEdit}>
+                <div className="container__CardList" key={this.props.note.id} >
+                    <div>
+                        <input className={'container__CardList__textEdit'} ref={(input) => this.getTitle = input} defaultValue={this.props.note.text} />
+                    </div>
+                    <div>
+                        <button className="container__CardList__Button" >
+                            <img alt='icon' className='container__CardList__Button_icon' src={edit}/>
+                        </button>
+                    </div>
+
+                </div>
+                </form>
             </div>
         );
     }
@@ -51,10 +55,7 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
     return bindActionCreators(
         {
-            deleteNote,
-            editNote,
             updateNote
-
         },
         dispatch
     );
