259f08e0977e2243e015f9a3c0e3e31fbd0e2126
Mohamadreza <m.reza.golbaba@gmail.com>
2019-12-19 17:12:38 +0330

    update reducers

--- src/util/Redux/reducers/NoteReducer.js
+++ src/util/Redux/reducers/NoteReducer.js
@@ -5,27 +5,21 @@ const INITIAL_DATA = [];
 const NOTEReducer = (state=INITIAL_DATA, action) => {
     switch (action.type){
         case ADD_NOTE:
-            return [
-                ...state,{
-                    id: action.id,
-                    text: action.text,
-                    editing:false,
-                }
-            ];
+            return state.concat([action.data]);
         case REMOVE_NOTE:
-            const numIndex = parseInt(action.id);
-            return state.filter(NOTE => NOTE.id !== numIndex);
+            return state.filter((post)=>post.id !== action.id);
         case EDIT_NOTE:
             return state.map((post)=>post.id === action.id ? {...post,editing:!post.editing}:post)
         case UPDATE_NOTE:
-            return [
-                ...state,{
-                    id: action.id,
-                    text: action.text,
-                    editing:false,
-                }
-            ];
-
+            return state.map((post)=>{
+                if(post.id === action.id) {
+                    return {
+                        ...post,
+                        text:action.data.newNote,
+                        editing: !post.editing
+                    }
+                } else return post;
+            });
         default:
             return state
     }
